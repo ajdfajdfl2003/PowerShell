756932dc3f0e1b956806af1b5d9f3181677450c0
ajdfajdfl <ajdfajdfl2003@gmail.com>
2017-12-29 14:04:14 +0800

    Finish let specific printer print test page

---
+++ ManagePrinters/PrintTestPage/PrintTestPage.ps1
@@ -0,0 +1,5 @@
+﻿Get-Printer
+$PrinterName = Read-Host 'Enter printer name as above of list'
+
+$PrinterInstance = [wmi]"\\.\root\cimv2:Win32_Printer.DeviceID='$PrinterName'"
+$PrinterInstance.PrintTestPage()
